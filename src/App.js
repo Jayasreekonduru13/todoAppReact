48914af5e7dcbfd8c8a49570ab0d7c0f7ba32edf
Jayasreekonduru13 <jayasreekonduru13@gmail.com>
2023-06-10 21:16:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from 'react';
+import './App.css'
 
-function App() {
+// Reducer function
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, { id: Date.now(), text: action.payload }];
+    case 'TOGGLE_TODO':
+      return state.map(todo =>
+        todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo
+      );
+    case 'DELETE_TODO':
+      return state.filter(todo => todo.id !== action.payload);
+    default:
+      return state;
+  }
+};
+const TodoApp = () => {
+  const [todos, dispatch] = useReducer(todoReducer, []);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleAddTodo = () => {
+    if (newTodo.trim()) {
+      dispatch({ type: 'ADD_TODO', payload: newTodo });
+      setNewTodo('');
+    }
+  };
+
+  const handleToggleTodo = id => {
+    dispatch({ type: 'TOGGLE_TODO', payload: id });
+  };
+
+  const handleDeleteTodo = id => {
+    dispatch({ type: 'DELETE_TODO', payload: id });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={e => setNewTodo(e.target.value)}
+        placeholder="Enter a new todo..."
+      />
+      <button className="button" onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map(todo => (
+          <li className="eachItem"
+            key={todo.id}
+            style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+          >
+            {todo.text}
+            <button className="button" onClick={() => handleToggleTodo(todo.id)}>Mark</button>
+            <button className="button" onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default TodoApp;
